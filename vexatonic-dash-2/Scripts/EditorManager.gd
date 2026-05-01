87a910587f8e8da6ec6949546cb835670a20e1a2
junu192857 <102847868+junu192857@users.noreply.github.com>
2026-05-01 20:57:02 +0900

    마우스 드래그로 에디터 화면 조작 가능

--- vexatonic-dash-2/Scripts/EditorManager.gd
+++ vexatonic-dash-2/Scripts/EditorManager.gd
@@ -5,3 +5,21 @@ var laneDatas: Array[Lane]
 
 @export var NOTE_SCENE: PackedScene
 @export var CONNECTOR_SCENE: PackedScene
+
+#Editor에서 Setting.speed는 1인 것으로 가정
+
+# ================== 마우스로 화면 드래그 =====================
+var dragging = false
+var drag_start: Vector2
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_RIGHT:
+			dragging = event.pressed
+			drag_start = event.position
+	
+	if event is InputEventMouseMotion and dragging:
+		var delta = event.position - drag_start
+		drag_start = event.position
+		$Camera2D.position -= delta
+	
