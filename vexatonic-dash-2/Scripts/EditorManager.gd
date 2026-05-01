d4310e76ddd71994b6ff83cc492dd3d6dc77d333
junu192857 <102847868+junu192857@users.noreply.github.com>
2026-05-01 23:44:14 +0900

    박자 구분선 출력 코드 시작

--- vexatonic-dash-2/Scripts/EditorManager.gd
+++ vexatonic-dash-2/Scripts/EditorManager.gd
@@ -30,4 +30,24 @@ func _on_zoom_camera(zoom: bool):
 			camera_zoom_level -= 1
 	var real_zoom = pow(1.2, camera_zoom_level)
 	camera.zoom = Vector2.ONE * real_zoom
-		
+# ===================== 박자 구분선 출력 =====================
+
+var music_bpm: Array[Vector2] # (time, bpm): time부터 bpm
+
+func calculate_camera_boundary() -> Vector4:
+	var viewport_size = get_viewport_rect().size / camera.zoom
+	var camera_pos = camera.global_position
+
+	var top_left = camera_pos - viewport_size / 2
+	var bottom_right = camera_pos + viewport_size / 2
+
+	var top = top_left.y
+	var bottom = bottom_right.y
+	var left = top_left.x
+	var right = bottom_right.x
+	
+	return Vector4(top, bottom, left, right)
+
+#현재 camera의 zoom과 position에 맞춰서 마디 구분선 출력
+func put_line():
+	pass
