490067eeb9bca072d4b878358267c742b3db5962
junu192857 <102847868+junu192857@users.noreply.github.com>
2026-05-02 23:57:03 +0900

    마디선 찍기 완료
    카메라 이동에 따른 움직임은 미구현

--- vexatonic-dash-2/Scripts/EditorManager.gd
+++ vexatonic-dash-2/Scripts/EditorManager.gd
@@ -5,6 +5,7 @@ var laneDatas: Array[Lane]
 
 @export var NOTE_SCENE: PackedScene
 @export var CONNECTOR_SCENE: PackedScene
+@export var LINE_SCENE: PackedScene
 
 @onready var inputHandler = $EditorInputHandler
 @onready var camera = $Camera2D
@@ -23,11 +24,16 @@ func _ready():
 
 func _on_start_editor():
 	editor_ready = true
-	var bpm = $CanvasLayer/InitialPanel/SpinBox.value
+	var bpm = $CanvasLayer/InitialPanel/BPMBox.value
+	var music_time = $CanvasLayer/InitialPanel/MusicTimeBox.value
+	if music_time == 0:
+		music_time = 180
 	$CanvasLayer/InitialPanel.visible = false
 	music_bpm.append(Vector2(0, bpm))
+	music_end_time = music_time * 1000
 	editorButtons.visible = true
 	print("BPM: %f" % bpm)
+	place_bar_lines()
 	
 
 # ================== 에디터 내 카메라 조작 =====================
@@ -53,31 +59,40 @@ func _on_zoom_camera(zoom: bool):
 # ===================== 박자 구분선 출력 =====================
 
 var music_bpm: Array[Vector2] # (time, bpm): time부터 bpm
+var music_end_time : float = 180000
+@onready var line_holder = $LineHolder
 
-func calculate_camera_boundary() -> Vector4:
-	var viewport_size = get_viewport_rect().size / camera.zoom
-	var camera_pos = camera.global_position
-
-	var top_left = camera_pos - 3 * viewport_size / 2
-	var bottom_right = camera_pos + 3 * viewport_size / 2
-
-	var top = top_left.y
-	var bottom = bottom_right.y
-	var left = top_left.x
-	var right = bottom_right.x
-	
-	return Vector4(top, bottom, left, right)
+#func calculate_camera_boundary() -> Vector4:
+#	var viewport_size = get_viewport_rect().size / camera.zoom
+#	var camera_pos = camera.global_position
+#
+#	var top_left = camera_pos - 3 * viewport_size / 2
+#	var bottom_right = camera_pos + 3 * viewport_size / 2
+#
+#	var top = top_left.y
+#	var bottom = bottom_right.y
+#	var left = top_left.x
+#	var right = bottom_right.x
+#	
+#	return Vector4(top, bottom, left, right)
 
 #현재 camera의 zoom과 position에 맞춰서 마디 구분선 출력
-func put_line():
-	var boundary = calculate_camera_boundary()
-	place_bar_lines(boundary)
-
-func place_bar_lines(boundary: Vector4):
+func place_bar_lines():
 	for i in range(music_bpm.size()):
 		var bpm_start_time = music_bpm[i].x
 		var bpm = music_bpm[i].y
-		var bpm_end_time = music_bpm[i + 1].x if i + 1 < music_bpm.size() else INF
+		var bpm_end_time = music_bpm[i + 1].x if i + 1 < music_bpm.size() else music_end_time
+		print(bpm_start_time)
+		print(bpm_end_time)
+		if bpm_start_time < 0:
+			push_error("time cannot be negative")
+			return
+		if bpm_start_time > bpm_end_time:
+			push_error("Please sort music_bpm by time ascending")
+			return
+		if bpm_end_time > music_end_time:
+			push_error("BPM end time cannot be later than music end time")
+			return
 
 		var beat_duration = 60000.0 / bpm
 		var bar_duration = beat_duration * 4
@@ -87,14 +102,11 @@ func place_bar_lines(boundary: Vector4):
 
 		while true:
 			var x = Setting.get_posx_from_time(time)
-			if x > boundary[3]:
-				break
 
-			if x >= boundary[2]:
-				if beat_count % 4 == 0:
-					put_line_1(x)
-				else:
-					put_line_2(x)
+			if beat_count % 4 == 0:
+				put_line(x, true)
+			else:
+				put_line(x, false)
 
 			beat_count += 1
 			time += beat_duration
@@ -104,11 +116,12 @@ func place_bar_lines(boundary: Vector4):
 
 		if i + 1 < music_bpm.size():
 			var x = Setting.get_posx_from_time(bpm_end_time)
-			if x >= boundary[2] and x <= boundary[3]:
-				put_line_1(x)
+			put_line(x, true)
 
-func put_line_1(pos_x: float):
-	pass
-
-func put_line_2(pos_x: float):
-	pass
+func put_line(pos_x: float, major: bool):
+	print("PUTTING LINE..")
+	var major_line = LINE_SCENE.instantiate()
+	line_holder.add_child(major_line)
+	major_line.position = Vector2(pos_x, camera.global_position.y)
+	major_line.scale = Vector2.ONE * pow(1.2, camera_zoom_level + 1 if major else camera_zoom_level)
+	
