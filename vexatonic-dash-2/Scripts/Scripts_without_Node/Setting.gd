862ea6da745248d77900ba26c211970657fd4ddf
junu192857 <102847868+junu192857@users.noreply.github.com>
2026-05-06 19:14:20 +0900

    일반 플레이에서 y좌표 관련 일관성 유지

--- vexatonic-dash-2/Scripts/Scripts_without_Node/Setting.gd
+++ vexatonic-dash-2/Scripts/Scripts_without_Node/Setting.gd
@@ -6,10 +6,10 @@ static var PX_PER_MS = 0.5
 #단노트의 좌우 길이
 static var NOTE_WIDTH = 24
 #Connector의 높이의 절반
-static var HALF_CONNECTOR_HEIGHT = 25
+static var HALF_CONNECTOR_HEIGHT = -25
 
 #레인 중앙 기준 캐릭터의 발 위치
-static var CHARACTER_POS_Y = 26
+static var CHARACTER_POS_Y = -26
 
 static func get_speed() -> float:
 	return speed
