5d8bbf321bd5e3a46c3c26f966ed0e25515c0b73
junu192857 <102847868+junu192857@users.noreply.github.com>
2026-07-06 23:02:40 +0900

    구조를 어느정도 짬

--- vexatonic-dash-2/Scripts/SelectSong/SelectManager.gd
+++ vexatonic-dash-2/Scripts/SelectSong/SelectManager.gd
@@ -1,4 +1,11 @@
-extends Node
+extends Node2D
+
+@export var leftSongHolder: SongHolder
+@export var middleSongHolder: SongHolder
+@export var rightSongHolder: SongHolder
+@export var songDataHolder: SongDataHolder
+@export var settingHolder: SettingHolder
+@onready var difficultyRect = $CanvasLayer/Control/DifficultyRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,6 +16,5 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 	
-func _load():
-	pass
-	
+func _on_game_start():
+	get_tree().change_scene_to_file("res://Scenes/RhythmScene.tscn")
--- vexatonic-dash-2/Scripts/SelectSong/SongHolder.gd
+++ vexatonic-dash-2/Scripts/SelectSong/SongHolder.gd
@@ -1,4 +1,4 @@
-extends Node
+extends TextureRect
 class_name SongHolder
 
 # Called when the node enters the scene tree for the first time.
