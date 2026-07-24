913ca9afe9703c57d8e8b3df8c1f06f4c55ffcc4
junu192857 <102847868+junu192857@users.noreply.github.com>
2026-07-24 22:51:06 +0900

    스크립트 수정

--- vexatonic-dash-2/Scripts/MainMenu/MainMenuManager.gd
+++ vexatonic-dash-2/Scripts/MainMenu/MainMenuManager.gd
@@ -55,6 +55,7 @@ func _on_game_start():
 
 func _on_tutorial_start():
 	Setting.is_tutorial = true
+	Setting.speed = 1.0
 	Setting.gamemode = Setting.GAMEMODE.Normal_Character
 	get_tree().change_scene_to_file("res://Scenes/TutorialScene.tscn")
 
