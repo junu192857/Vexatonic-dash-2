8c129d3fa8287642490cf798c236d9c31371f875
junu192857 <102847868+junu192857@users.noreply.github.com>
2026-08-08 22:58:31 +0900

    SettingRect 폰트 수정

--- vexatonic-dash-2/Scripts/Setting/SettingRectManager.gd
+++ vexatonic-dash-2/Scripts/Setting/SettingRectManager.gd
@@ -6,7 +6,7 @@ enum SettingItem { Speed, SoundOffset, JudgeOffset, Gamemode, ScoreDisplay }
 var setting_index: int = 0
 
 const SETTING_INFO = [
-	"노트 속도를 설정합니다.",
+	"노트 속도를 설정합니다.\n1.0~3.0 사이의 값을 권장합니다.",
 	"음악 싱크를 설정합니다.\nFast가 많다면 (+)방향, Late가 많다면 (-)방향으로 조절하세요.",
 	"판정 싱크를 설정합니다.\nFast가 많다면 (+)방향, Late가 많다면 (-)방향으로 조절하세요.",
 	"게임 모드를 설정합니다.",
