f3e4be80fb3baca94a37606f52411be1bd5fe760
junu192857 <102847868+junu192857@users.noreply.github.com>
2026-08-23 23:21:18 +0900

    SelectSong 씬에서 음악 재생

--- vexatonic-dash-2/Scripts/SelectSong/SelectManager.gd
+++ vexatonic-dash-2/Scripts/SelectSong/SelectManager.gd
@@ -15,6 +15,8 @@ extends Node2D
 @export var settingRectScene: PackedScene
 var settingRect
 
+@onready var musicPlayer: AudioStreamPlayer = $MusicPlayer
+
 const DIFFICULTY_COLORS: Array[Color] = [
   Color(0.6, 1.0, 0.4),
   Color(1.0, 0.65, 0.2),
@@ -62,6 +64,7 @@ func _ready() -> void:
 				current_index = i
 				break
 	_refresh_all()
+	_play_current_song_music()
 
 func _ensure_user_charts():
 	# user://Charts가 이미 있으면 건너뜀
@@ -203,6 +206,20 @@ func _on_slide_finished():
 	_refresh_can_start()
 	songSelectionHolder.position.x = slide_origin_x
 	is_animating = false
+	_play_current_song_music()
+
+func _play_current_song_music() -> void:
+	var meta = _get_metadata(0)
+	var chart_dir = CHARTS_DIR + "/" + meta.name
+	var music_path = chart_dir + "/" + meta.music_path
+	if not FileAccess.file_exists(music_path):
+		musicPlayer.stop()
+		return
+	var stream := AudioStreamMP3.new()
+	stream.data = FileAccess.get_file_as_bytes(music_path)
+	stream.loop = true
+	musicPlayer.stream = stream
+	musicPlayer.play()
 
 func _refresh_can_start():
 	if (_can_start()):
